037eeaebf1ed11c5f4da3bda847fbd3e3b33058d
Doige_Sirius <agu-amd@hotmail.com>
2020-07-20 23:03:05 -0300

    Added the END macro

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define DBG(x) cout << #x << " = " << (x) << "\n"
+#define END(e) {cout << (e) << "\n"; return EXIT_SUCCESS;}
 
 int main() {
     ios::sync_with_stdio(false);
