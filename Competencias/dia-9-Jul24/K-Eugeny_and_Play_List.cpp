e08403d77431c235ec32a6bdbf747aa3ad695d66
Doige_Sirius <agu-amd@hotmail.com>
2020-07-25 01:07:34 -0300

    K

--- Competencias/dia-9-Jul24/K-Eugeny_and_Play_List.cpp
+++ Competencias/dia-9-Jul24/K-Eugeny_and_Play_List.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int n, m, c, t, v, song, tme;
+int n, m, c, t, v, cancion, tiempo;
  
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
-    int list[n+5];
-    list[0] = 0;
+    int playlist[n+5];
+    playlist[0] = 0;
     for (int i=1; i<=n; ++i) {
         cin >> c >> t;
-        list[i] = c*t;
+        playlist[i] = c*t;
     }
     for (int i=0; i<m; ++i) {
         cin >> v;
-        while (tme<v) {
-            ++song;
-            tme += list[song];
+        while (tiempo<v) {
+            ++cancion;
+            tiempo += playlist[cancion];
         }
-        cout << song << "\n";
+        cout << cancion << "\n";
     }
 }
